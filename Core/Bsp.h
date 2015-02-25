3affbe49ddc478381f7fce994edd9674f1737725
golgepapaz <hsolter@gmail.com>
2015-02-26 01:58:19 +0200

    BSPTree

--- Core/Bsp.h
+++ Core/Bsp.h
@@ -3,9 +3,21 @@
 
 struct BSPNode
 {
+    BSPNode(int x, int y, int w, int h) : x(x), y(y), w(w), h(h), parent(nullptr), left(nullptr), right(nullptr)
+    {}
     BSPNode *parent;
     BSPNode *left;
     BSPNode *right;
     int x, y, w, h;
+
+};
+class BSPTree
+{
+    BSPNode  *root;
+public:
+    BSPTree(int x, int y, int w, int h)
+    {
+        root = new BSPNode(x, y, w, h);
+    }
 
 };
--- Core/Random.h
+++ Core/Random.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include <random>
+#include <chrono>
 class Randomizer
 {
     static std::default_random_engine generator;
 public:
+    Randomizer()
+    {
+        std::random_device sd;
+        generator.seed(sd);
+    }
     static int RandInt(int min, int max)
     {
         std::uniform_int_distribution<int> dist(min, max);
@@ -12,7 +18,7 @@ public:
     }
     static float RandFloat(float min, float max)
     {
-        std::uniform_real_distribution<float> dist(min, max)
+        std::uniform_real_distribution<float> dist(min, max);
         return dist(generator);
     }
 
