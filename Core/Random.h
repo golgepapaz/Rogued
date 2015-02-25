97be27829d9d7cdbd458dd5f621ea4832de05a71
hurcan solter <hsolter@gmail.com>
2015-02-25 17:05:20 +0200

    lolll

--- Core/Random.h
+++ Core/Random.h
@@ -1,4 +1,10 @@
 #pragma once
 
 #include <random>
+namespace Random
+{
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine gen(seed);
+    std::uniform_distribution<int>
 
+}
