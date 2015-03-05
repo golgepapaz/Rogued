ca672730cd37ac85250d22fd195248218eb03903
golgepapaz <hsolter@gmail.com>
2015-03-06 01:16:18 +0200

    lol

---
+++ Source/MyRogue/DungeonVisualizer.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyRogue.h"
+#include "DungeonVisualizer.h"
+
+ADungeonVisualizer::ADungeonVisualizer(const FObjectInitializer& init) : Super(init)
+{
+
+}
+
+
+
+
---
+++ Source/MyRogue/DungeonVisualizer.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/HUD.h"
+#include "LevelGenerator.h"
+#include "DungeonVisualizer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYROGUE_API ADungeonVisualizer : public AHUD
+{
+    GENERATED_BODY()
+public:
+
+        UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parameters)
+        int32 X;
+        UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Parameters)
+        int32 Y;
+
+        ADungeonVisualizer(const FObjectInitializer& init);
+	
+};
